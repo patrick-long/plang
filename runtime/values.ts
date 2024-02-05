361146d7262986d7f00b15b7f8134dcbded3c35d
Patrick Long <patrick@deepspacerobots.com>
2024-02-05 11:15:08 -0600

    create macroes for a NullValue, NumberValue and BooleanValue

--- runtime/values.ts
+++ runtime/values.ts
@@ -1,4 +1,4 @@
-export type ValueType = "null" | "number";
+export type ValueType = "null" | "number" | "boolean";
 
 export interface RuntimeValue {
 	type: ValueType;
@@ -6,10 +6,27 @@ export interface RuntimeValue {
 
 export interface NullValue extends RuntimeValue {
 	type: "null";
-	value: "null";
+	value: null;
 }
 
 export interface NumberValue extends RuntimeValue {
 	type: "number";
 	value: number;
+}
+
+export interface BooleanValue extends RuntimeValue {
+	type: "boolean";
+	value: boolean;
+}
+
+export function MAKE_NULL(): NullValue {
+	return { type: "null", value: null };
+}
+
+export function MAKE_NUMBER(value = 0): NumberValue {
+	return { type: "number", value };
+}
+
+export function MAKE_BOOL(value = true): BooleanValue {
+	return { type: "boolean", value };
 }
